ec194158b640fd9f6be93cc30f6f82385f161c0c
Venkata Duvvuri <venkata.r.duvvuri@gmail.com>
2018-09-02 23:01:57 -0700

    Rename directories

---
+++ 2018-09AddingTransitionFromRecoveredState-To-SusceptibleState/module-fx.R
@@ -0,0 +1,147 @@
+
+##
+## SEIR Model: Adding an Exposed State to an SIR
+## EpiModel Gallery (https://github.com/statnet/EpiModel-Gallery)
+##
+## Author: Samuel M. Jenness (Emory University)
+## Contributor: Venkata R. Duvvuri
+## Date: Sep 2018
+##
+
+
+# Replacement infection/transmission module -------------------------------
+
+infect <- function(dat, at) {
+
+  ## Uncomment this to function environment interactively
+  # browser()
+
+  ## Attributes ##
+  active <- dat$attr$active
+  status <- dat$attr$status
+
+  ## Parameters ##
+  inf.prob <- dat$param$inf.prob
+  act.rate <- dat$param$act.rate
+
+  ## Find infected nodes ##
+  idsInf <- which(active == 1 & status == "i")
+  nActive <- sum(active == 1)
+  nElig <- length(idsInf)
+
+  ## Initialize default incidence at 0 ##
+  nInf <- 0
+
+  ## If any infected nodes, proceed with transmission ##
+  if (nElig > 0 && nElig < nActive) {
+
+    ## Look up discordant edgelist ##
+    del <- discord_edgelist(dat, at)
+
+    ## If any discordant pairs, proceed ##
+    if (!(is.null(del))) {
+
+      # Set parameters on discordant edgelist data frame
+      del$transProb <- inf.prob
+      del$actRate <- act.rate
+      del$finalProb <- 1 - (1 - del$transProb)^del$actRate
+
+      # Stochastic transmission process
+      transmit <- rbinom(nrow(del), 1, del$finalProb)
+
+      # Keep rows where transmission occurred
+      del <- del[which(transmit == 1), ]
+
+      # Look up new ids if any transmissions occurred
+      idsNewInf <- unique(del$sus)
+      nInf <- length(idsNewInf)
+
+      # Set new attributes for those newly infected
+      if (nInf > 0) {
+        dat$attr$status[idsNewInf] <- "e"
+        dat$attr$infTime[idsNewInf] <- at
+        
+        
+      }
+    }
+  }
+
+  ## Save summary statistic for S->E flow
+  dat$epi$se.flow[at] <- nInf
+
+  return(dat)
+}
+
+
+# New disease progression module ------------------------------------------
+# (Replaces the recovery module)
+
+progress <- function(dat, at) {
+
+  ## Uncomment this to function environment interactively
+  # browser()
+
+  ## Attributes ##
+  active <- dat$attr$active
+  status <- dat$attr$status
+
+  ## Parameters ##
+  ei.rate <- dat$param$ei.rate
+  ir.rate <- dat$param$ir.rate
+  rs.rate <- dat$param$rs.rate
+
+  ## E to I progression process ##
+  nInf <- 0
+  idsEligInf <- which(active == 1 & status == "e")
+  nEligInf <- length(idsEligInf)
+
+  if (nEligInf > 0) {
+    vecInf <- which(rbinom(nEligInf, 1, ei.rate) == 1)
+    if (length(vecInf) > 0) {
+      idsInf <- idsEligInf[vecInf]
+      nInf <- length(idsInf)
+      status[idsInf] <- "i"
+    }
+  }
+
+  ## I to R progression process ##
+  nRec <- 0
+  idsEligRec <- which(active == 1 & status == "i")
+  nEligRec <- length(idsEligRec)
+
+  if (nEligRec > 0) {
+    vecRec <- which(rbinom(nEligRec, 1, ir.rate) == 1)
+    if (length(vecRec) > 0) {
+      idsRec <- idsEligRec[vecRec]
+      nRec <- length(idsRec)
+      status[idsRec] <- "r"
+    }
+  }
+  
+  # ## R to S progression process ##
+  nSus <- 0
+  idsEligSus <- which(active == 1 & status == "r")
+  nEligSus <- length(idsEligSus)
+
+  if (nEligSus > 0) {
+    vecSus <- which(rbinom(nEligSus, 1, rs.rate) == 1)
+    if (length(vecSus) > 0) {
+      idsSus <- idsEligSus[vecSus]
+      nSus <- length(idsSus)
+      status[idsSus] <- "s"
+    }
+  }
+
+  ## Write out updated status attribute ##
+  dat$attr$status <- status
+
+  ## Save summary statistics ##
+  dat$epi$ei.flow[at] <- nInf
+  dat$epi$ir.flow[at] <- nRec
+  dat$epi$rs.flow[at] <- nSus
+  dat$epi$e.num[at] <- sum(active == 1 & status == "e")
+  dat$epi$r.num[at] <- sum(active == 1 & status == "r")
+  dat$epi$s.num[at] <- sum(active == 1 & status == "s")
+
+  return(dat)
+}
