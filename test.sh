78dba8a3ec76dc95326211eda988f2696bf74379
Samuel Jenness <smjenness@gmail.com>
2018-10-26 14:45:39 -0400

    Update test script output

--- test.sh
+++ test.sh
@@ -6,6 +6,12 @@ d="$(ls -p | grep "/")"
 
 for i in $d; do
   cd $i
+  echo " "
+  echo "#############################################"
+  echo "RUNNING MODEL"
+  echo $i
+  echo "#############################################"
+  echo " "
   Rscript model.R "options(error = function() q('no', 1, FALSE))"
   rm *.pdf
   cd ..
