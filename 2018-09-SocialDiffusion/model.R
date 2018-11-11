da82e73fb0224e68388846b074fc9fcdf9b6af9f
Samuel Jenness <smjenness@gmail.com>
2018-11-11 15:16:33 -0500

    Minor coding edits

--- 2018-09-SocialDiffusion/model.R
+++ 2018-09-SocialDiffusion/model.R
@@ -1,16 +1,19 @@
+
 ##
-## Information Diffusion Model in a Social Network: Adding a minimum number of degree to S in an SI Model
+## Modeling Social Diffusion in a Social Network
 ## EpiModel Gallery (https://github.com/statnet/EpiModel-Gallery)
 ##
 ## Author: Yuan Zhao
-## Date: September 2018
+## Date: November 2018
 ##
 
 # Load EpiModel
 suppressMessages(library(EpiModel))
+
 # Standard Gallery unit test lines
 rm(list = ls())
 eval(parse(text = print(commandArgs(TRUE)[1])))
+
 
 # Network model estimation ------------------------------------------------
 
@@ -37,11 +40,12 @@ print(dx)
 plot(dx, plots.joined = FALSE, qnts.alpha = 0.8)
 
 
-# Scenario 1: Simulation of minimum degree of disordant edgelist -------------------------------------------
+# Scenario 1 --------------------------------------------------------------
 
 # Model parameters
-param <- param.net(inf.prob = 0.5, act.rate = 2,
-                    min.degree=3)
+param <- param.net(inf.prob = 0.5,
+                   act.rate = 2,
+                   min.degree = 3)
 
 # Initial conditions
 init <- init.net(i.num = 100)
@@ -50,15 +54,14 @@ init <- init.net(i.num = 100)
 source("module-fx.R", echo = TRUE)
 
 # Control settings
-control <- control.net(type="SI",
+control <- control.net(type = "SI",
                        nsteps = 300,
-                       nsims = 8,
-                       ncores = 4,
-                       infection.FUN = infect_mod
-)
+                       nsims = 4,
+                       ncores = 2,
+                       infection.FUN = diffuse_mod)
+
 
 # Run the network model simulation with netsim
-set.seed(123456)
 sim <- netsim(est, param, init, control)
 print(sim)
 
@@ -69,9 +72,9 @@ plot(sim,
      qnts = 1, qnts.col = 1:2, qnts.alpha = 0.25, qnts.smooth = FALSE,
      legend = TRUE)
 
-plot(sim, y = c("si.flow"),
-     mean.col = 1:2, mean.lwd = 1, mean.smooth = TRUE,
-     qnts.col = 1:2, qnts.alpha = 0.25, qnts.smooth = TRUE,
+plot(sim, y = "si.flow",
+     mean.col = 1, mean.lwd = 1, mean.smooth = TRUE,
+     qnts.col = 1, qnts.alpha = 0.25, qnts.smooth = TRUE,
      ylim = c(0, 6), legend = TRUE)
 
 # Average across simulations at beginning, middle, and end
@@ -79,31 +82,23 @@ df <- as.data.frame(sim)
 df[c(2, 50, 100, 290), ]
 
 
+# Scenario 2 --------------------------------------------------------------
 
-# Plot network
-par(mar = c(3,3,1,1), mgp = c(2,1,0))
-
-plot(sim,type="network",at=2,sims="mean",
-     col.status=TRUE, main="Network at t2")
-plot(sim,type="network",at=150,sims="mean",
-     col.status=TRUE, main="Network at t150")
-
-
-## Scenario 2: the infection probility as the function of discordant degree -------------------------------------------
 # Model parameters
-param <- param.net(act.rate = 2, beta0=-7, beta1=0.5)
+param <- param.net(act.rate = 2,
+                   beta0 = -7,
+                   beta1 = 0.5)
 
 # Initial conditions
 init <- init.net(i.num = 100)
 
+# Control settings
 control <- control.net(nsteps = 300,
-                       nsims = 8,
-                       ncores = 4,
-                       infection.FUN = infect_newmod
-)
+                       nsims = 4,
+                       ncores = 2,
+                       infection.FUN = diffuse_mod2)
 
 # Run the network model simulation with netsim
-set.seed(123456)
 sim <- netsim(est, param, init, control)
 print(sim)
 
@@ -114,20 +109,11 @@ plot(sim,
      qnts = 1, qnts.col = 1:2, qnts.alpha = 0.25, qnts.smooth = FALSE,
      legend = TRUE)
 
-plot(sim, y = c("si.flow"),
-     mean.col = 1:2, mean.lwd = 1, mean.smooth = TRUE,
-     qnts.col = 1:2, qnts.alpha = 0.25, qnts.smooth = TRUE,
-     ylim = c(0, 3), legend = TRUE)
+plot(sim, y = "si.flow",
+     mean.col = 1, mean.lwd = 1, mean.smooth = TRUE,
+     qnts.col = 1, qnts.alpha = 0.25, qnts.smooth = TRUE,
+     ylim = c(0, 2), legend = TRUE)
 
-# Average across simulations at beginning, middle, and the end 
+# Average across simulations at beginning, middle, and the end
 df <- as.data.frame(sim)
 df[c(2, 100, 200, 300), ]
-
-
-
-# Plot network
-par(mar = c(3,3,1,1), mgp = c(2,1,0))
-plot(sim,type="network",at=10,sims="mean",
-     col.status=TRUE, main="Prevalence at t10")
-plot(sim,type="network",at=100,sims="mean",
-     col.status=TRUE, main="Prevalence at t100")
--- 2018-09-SocialDiffusion/module-fx.R
+++ 2018-09-SocialDiffusion/module-fx.R
@@ -1,142 +1,149 @@
 # Scenario 1: Minimum degree transmission pattern
 # Update Transmission Module ----------------------------------------------
 
-infect_mod <- function(dat, at) {
-      
+diffuse_mod <- function(dat, at) {
+
   ## Attributes ##
   active <- dat$attr$active
   status <- dat$attr$status
-  
+
   ## Parameters ##
   inf.prob <- dat$param$inf.prob
   act.rate <- dat$param$act.rate
   min.degree <- dat$param$min.degree
-  
+
   # Vector of infected and susceptible IDs
   idsInf <- which(active == 1 & status == "i")
   nActive <- sum(active == 1)
   nElig <- length(idsInf)
-  
+
   # Initialize vectors at 0
   nInf <- totInf <- 0
- 
+
   ## Processes ##
   # If some infected AND some susceptible, then proceed
   if (nElig > 0 && nElig < nActive) {
 
     # Get discordant edgelist
     del <- discord_edgelist(dat, at)
-    #browser()
+
     # If some discordant edges, then proceed
     if (!(is.null(del))) {
-      # Infection probabilities
-      # browser()
-      del$degree<-1
-      subdel<-aggregate(degree~sus,FUN=length,data=del)
-      del<-merge(del[, 1:3], subdel, by = "sus", all=TRUE)
-      
-      # If some susceptible nodes have more than minimum degree with infected person, 
+
+      # Initialize degree variable on edgelist
+      del$degree <- 1
+
+      # Count degree in the del
+      subdel <- aggregate(degree ~ sus, FUN = length, data = del)
+
+      # Merge new degree count in
+      del <- merge(del[, 1:3], subdel, by = "sus", all = TRUE)
+
+      # If some susceptible nodes have more than minimum degree with infected person,
       # then set their transmission prob as transmission probility
-      # browser()
       ## Test if work for no one having more than min degree
       del$transProb <- ifelse(del$degree >= min.degree, inf.prob, 0)
-      
+
       # Act rates
       del$actRate <- act.rate
-      
+
       # Calculate final transmission probability per timestep
       del$finalProb <- 1 - (1 - del$transProb) ^ del$actRate
-      
+
       # Randomize transmissions and subset df
       transmit <- rbinom(nrow(del), 1, del$finalProb)
       del <- del[which(transmit == 1), ]
-      
+
       # Set new infections vector
       idsNewInf <- unique(del$sus)
       totInf <- length(idsNewInf)
-      
+
       # Update attributes for newly infected
       if (totInf > 0) {
         dat$attr$status[idsNewInf] <- "i"
         dat$attr$infTime[idsNewInf] <- at
       }
-      
+
     }
   }
-  
+
   ## Summary statistics ##
   dat$epi$si.flow[at] <- totInf
-  
+
   return(dat)
 }
 
 # Scenario 2: Transmission probability as a function of degree of discordant edgelist
 
-infect_newmod <- function(dat, at) {
-  
+diffuse_mod2 <- function(dat, at) {
+
   ## Attributes ##
   active <- dat$attr$active
   status <- dat$attr$status
-  
+
   ## Parameters ##
   beta0 <- dat$param$beta0
   beta1 <- dat$param$beta1
   act.rate <- dat$param$act.rate
-  
+
   # Vector of infected and susceptible IDs
   idsInf <- which(active == 1 & status == "i")
   nActive <- sum(active == 1)
   nElig <- length(idsInf)
-  
+
   # Initialize vectors at 0
   nInf <- totInf <- 0
-  
+
   ## Processes ##
   # If some infected AND some susceptible, then proceed
   if (nElig > 0 && nElig < nActive) {
-    #browser()
+
     # Get discordant edgelist
     del <- discord_edgelist(dat, at)
-    
+
     # If some discordant edges, then proceed
     if (!(is.null(del))) {
-      # get discordant degree for susceptible individuals
-      del$degree<-1
-      subdel<-aggregate(degree~sus,FUN=length,data=del)
-      del<-merge(del[, 1:3], subdel, by = "sus", all=TRUE)
-      
+
+      # Initialize degree variable on edgelist
+      del$degree <- 1
+
+      # Count degree in the del
+      subdel <- aggregate(degree ~ sus, FUN = length, data = del)
+
+      # Merge new degree count in
+      del <- merge(del[, 1:3], subdel, by = "sus", all = TRUE)
+
       # The probability of infection is logistic function of susceptible nodes' degree with infected nodes
       # With parameters of beta0 and beta1
-      # browser()
       ## beta1: log odds ratio with 1 degree increase of discordant relationship
       ## beta0: baseline log odds of transmission when degree of discordant edgelist is 0
-      del$transProb <- plogis(beta0+beta1*del$degree)
-      
+      del$transProb <- plogis(beta0 + beta1*del$degree)
+
       # Act rates
       del$actRate <- act.rate
-      
+
       # Calculate final transmission probability per timestep
       del$finalProb <- 1 - (1 - del$transProb) ^ del$actRate
-      
+
       # Randomize transmissions and subset df
       transmit <- rbinom(nrow(del), 1, del$finalProb)
       del <- del[which(transmit == 1), ]
-      
+
       # Set new infections vector
       idsNewInf <- unique(del$sus)
       totInf <- length(idsNewInf)
-      
+
       # Update attributes for newly infected
       if (totInf > 0) {
         dat$attr$status[idsNewInf] <- "i"
         dat$attr$infTime[idsNewInf] <- at
       }
-      
+
     }
   }
-  
+
   ## Summary statistics ##
   dat$epi$si.flow[at] <- totInf
-  
+
   return(dat)
 }
