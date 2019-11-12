83e5d657e9c71d3fba441341ab39cc6333893c26
smjenness <smjenness@gmail.com>
2019-11-12 13:29:43 -0500

    Update syphilis model

--- 2018-11-Syphilis/model.R
+++ 2018-11-Syphilis/model.R
@@ -13,6 +13,16 @@ suppressMessages(library(EpiModel))
 rm(list = ls())
 eval(parse(text = print(commandArgs(TRUE)[1])))
 
+if (interactive()) {
+  nsims <- 5
+  ncores <- 5
+  nsteps <- 500
+} else {
+  nsims <- 1
+  ncores <- 1
+  nsteps <- 50
+}
+
 # Network model estimation ------------------------------------------------
 
 # Initialize the network
@@ -33,107 +43,99 @@ coef.diss
 est <- netest(nw, formation, target.stats, coef.diss)
 
 # Model diagnostics
-dx <- netdx(est, nsims = 8, ncores = 8, nsteps = 500,
-            nwstats.formula = ~edges + isolates + degree(0:5))
+dx <- netdx(est, nsims = nsims, ncores = ncores, nsteps = nsteps,
+            nwstats.formula = ~edges + isolates + degree(0:3))
 print(dx)
-plot(dx, plots.joined = FALSE, qnts.alpha = 0.8)
+plot(dx)
 
 
 # Epidemic model simulation -----------------------------------------------
 
 # Model parameters
-param <- param.net(inf.prob1 = 0.18, 
-                   inf.prob2 = 0.09, 
+param <- param.net(inf.prob1 = 0.18,
+                   inf.prob2 = 0.09,
                    act.rate = 2,
-                   ipr.rate = 1/4, 
-                   prse.rate = 1/9, 
+                   ipr.rate = 1/4,
+                   prse.rate = 1/9,
                    seel.rate = 1/17,
                    elll.rate = 1/22,
-                   llter.rate = 1/1508, 
-                   pri.sym = 0.205, 
-                   sec.sym = 0.106, 
-                   early.trt = 0.8, 
-                   late.trt = 1.0, 
+                   llter.rate = 1/1508,
+                   pri.sym = 0.205,
+                   sec.sym = 0.106,
+                   early.trt = 0.8,
+                   late.trt = 1.0,
                    scr.rate = 1/52)
-
-## transmission probability (per-act) inf.prob1 = 0.18: incubating, primary, 
-## and secondary stages; inf.prob2 = 0.09: Early latent;
 
 # Initial conditions
 init <- init.net(i.num = 10)
 
 # Read in the module functions
-source("module-fx.R", echo = TRUE)
+if (interactive()) {
+  source("2018-11-Syphilis/module-fx.R", echo = TRUE)
+} else {
+  source("module-fx.R")
+}
 
 # Control settings
-control <- control.net(nsteps = 400,
-                       nsims = 4,
-                       ncores = 1,
+control <- control.net(nsteps = nsteps,
+                       nsims = nsims,
+                       ncores = ncores,
                        infection.FUN = infect,
                        progress.FUN = progress,
                        tnt.FUN = tnt)
 
 # Run the network model simulation with netsim
-set.seed(123456)
 sim <- netsim(est, param, init, control)
 print(sim)
 
 # Examine data from simulations
 df <- as.data.frame(sim)
-df[c(2, 100, 400), ]
+filter(df, sim == 1 & time %in% c(2, 25, 50))
 
 ## Plot outcomes
 
 # All compartments
 par(mar = c(3,3,1,1), mgp = c(2,1,0))
-plot(sim,y = c("s.num", "i.num", "inc.num","pr.num","se.num","el.num", "ll.num", 
-               "ter.num"), 
+plot(sim,y = c("s.num", "i.num", "inc.num", "pr.num",
+               "se.num", "el.num", "ll.num", "ter.num"),
      mean.col = 1:8, mean.lwd = 1, mean.smooth = FALSE,
      qnts = 1, qnts.col = 1:8, qnts.alpha = 0.25, qnts.smooth = FALSE,
      legend = TRUE)
 
 # SI compartment counts
 par(mar = c(3,3,1,1), mgp = c(2,1,0))
-plot(sim,y = c("s.num", "i.num"), mean.col = 1:2, mean.lwd = 1, 
-     mean.smooth = FALSE, qnts = 1, qnts.col = c(1:2), qnts.alpha = 0.25, 
+plot(sim,y = c("s.num", "i.num"), mean.col = 1:2, mean.lwd = 1,
+     mean.smooth = FALSE, qnts = 1, qnts.col = c(1:2), qnts.alpha = 0.25,
      qnts.smooth = FALSE, legend = TRUE)
 
 # Syphilis stage compartment counts
 par(mar = c(3,3,1,1), mgp = c(2,1,0))
-plot(sim,y = c("inc.num","pr.num","se.num","el.num", "ll.num", "ter.num"), 
+plot(sim,y = c("inc.num", "pr.num", "se.num",
+               "el.num", "ll.num", "ter.num"),
      mean.col = 3:8, mean.lwd = 1, mean.smooth = FALSE,
      qnts = 1, qnts.col = 3:8, qnts.alpha = 0.25, qnts.smooth = FALSE)
-legend("topleft", c("inc.num","pr.num","se.num","el.num", "ll.num", "ter.num"),
-       col = c(3:8), lty = 1, cex = 0.8)
+legend("topleft", c("inc.num","pr.num","se.num",
+                    "el.num", "ll.num", "ter.num"),
+       col = 3:8, lty = 1, cex = 0.8)
 
 # Incidence/compartment flow rates
-plot(sim, y = c("si.flow", "ipr.flow", "prse.flow","seel.flow", "elll.flow", 
-                "llter.flow"), mean.col = 1:6, mean.lwd = 1, mean.smooth = TRUE,
-     qnts.col = 1:6, qnts.alpha = 0.25, qnts.smooth = TRUE, ylim = c(0,10),
-     legend = TRUE)
+plot(sim, y = c("si.flow", "ipr.flow", "prse.flow",
+                "seel.flow", "elll.flow", "llter.flow"),
+     mean.col = 1:6, mean.lwd = 1, mean.smooth = TRUE,
+     qnts.col = 1:6, qnts.alpha = 0.25, qnts.smooth = TRUE,
+     ylim = c(0, 10), legend = TRUE)
 
 # Plot screening rate
-plot(sim, y = c("scr.flow"), mean.col = 1, mean.lwd = 1, mean.smooth = TRUE,
+plot(sim, y = "scr.flow", mean.col = 1, mean.lwd = 1, mean.smooth = TRUE,
      qnts.col = 1, qnts.alpha = 0.25, qnts.smooth = TRUE, legend = TRUE)
 
 # Duration spent in each stage of infection
-plot(sim, y = c("syph.dur","syph2.dur","syph3.dur","syph4.dur","syph5.dur",
-                "syph6.dur"), mean.col = 1:6, mean.lwd = 1, mean.smooth = TRUE,
-     qnts.col = 1:6, qnts.alpha = 0.25, qnts.smooth = TRUE, ylim = c(0,40),
+plot(sim, y = c("syph.dur", "syph2.dur", "syph3.dur", "syph4.dur"),
+     mean.col = 1:4, mean.lwd = 1, mean.smooth = TRUE,
+     qnts.col = 1:4, qnts.alpha = 0.25, qnts.smooth = TRUE, ylim = c(0,40),
      legend = TRUE)
 
-## Plot without y limit
-plot(sim, y = c("syph.dur","syph2.dur","syph3.dur","syph4.dur","syph5.dur",
-                "syph6.dur"),mean.col = 1:6, mean.lwd = 1, mean.smooth = TRUE,
-     qnts.col = 1:6, qnts.alpha = 0.25, qnts.smooth = TRUE)
-legend("topleft", legend = c("syph.dur","syph2.dur","syph3.dur","syph4.dur",
-                             "syph5.dur","syph6.dur"), col = c(1:8), lty = 1,
-       cex = 0.8)
-
 # Prevalence of symptoms
-plot(sim, y = c("sym.num"),
-     mean.col = 1, mean.lwd = 1, mean.smooth = TRUE, qnts.col = 1, 
+plot(sim, y = "sym.num",
+     mean.col = 1, mean.lwd = 1, mean.smooth = TRUE, qnts.col = 1,
      qnts.alpha = 0.25, qnts.smooth = TRUE, legend = TRUE)
-
-
-
