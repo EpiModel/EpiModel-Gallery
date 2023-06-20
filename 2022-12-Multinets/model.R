d6d671398c53f94ecc3689937e719f4305098119
Samuel Jenness <samuel.m.jenness@emory.edu>
2023-06-20 12:46:38 -0400

    Update model.R

--- 2022-12-Multinets/model.R
+++ 2022-12-Multinets/model.R
@@ -14,6 +14,16 @@ suppressMessages(library(EpiModel))
 rm(list = ls())
 eval(parse(text = print(commandArgs(TRUE)[1])))
 
+if (interactive()) {
+  nsims <- 5
+  ncores <- 5
+  nsteps <- 500
+} else {
+  nsims <- 2
+  ncores <- 2
+  nsteps <- 100
+}
+
 # Simulate data consistent with the target statistics we will use
 nw <- network_initialize(500)
 nw <- set_vertex_attribute(nw, "race", rep(c(0,1), length.out = 500))
@@ -54,11 +64,11 @@ est.1 <- netest(nw, formation.1, target.stats.1, coef.diss.1)
 est.2 <- netest(nw, formation.2, target.stats.2, coef.diss.2)
 
 # Model diagnostics
-dx.1 <- netdx(est.1, nsims = 4, ncores = 4, nsteps = 500)
+dx.1 <- netdx(est.1, nsims = nsims, ncores = ncores, nsteps = nsteps)
 print(dx.1)
 plot(dx.1)
 
-dx.2 <- netdx(est.2, nsims = 4, ncores = 4, nsteps = 500)
+dx.2 <- netdx(est.2, nsims = nsims, ncores = ncores, nsteps = nsteps)
 print(dx.2)
 plot(dx.2)
 
@@ -72,30 +82,37 @@ param <- param.net(inf.prob = 0.5, act.rate = 2,
 init <- init.net(i.num = 10)
 
 # Control settings
+
+# This function defines activities that should occur between resimulation of individual
+#   network layers
+network_layer_updates <- function(dat, at, network) {
+  if (network == 0) {
+    # update deg1+.net2 prior to network 1 simulation,
+    # as network 1's formation model depends on this
+    # nodal attribute
+    dat <- set_attr(dat, "deg1+.net2", pmin(get_degree(dat$el[[2]]), 1))
+  } else if (network == 1) {
+    # update deg1+.net1 prior to network 2 simulation,
+    # as network 2's formation model depends on this
+    # nodal attribute
+    dat <- set_attr(dat, "deg1+.net1", pmin(get_degree(dat$el[[1]]), 1))
+  } else if (network == 2) {
+    # update deg1+.net2 after network 2 simulation,
+    # for summary statistics
+    dat <- set_attr(dat, "deg1+.net2", pmin(get_degree(dat$el[[2]]), 1))
+  }
+  return(dat)
+}
+
+# Note the nwstats.formula uses the multilayer function to define the stats
+#   specific to each layer
 control <- control.net(type = "SI",
-                       nsteps = 500,
-                       nsims = 4,
-                       ncores = 4,
+                       nsteps = nsteps,
+                       nsims = nsims,
+                       ncores = ncores,
                        tergmLite = TRUE,
                        resimulate.network = TRUE,
-                       dat.updates = function(dat, at, network) {
-                                       if (network == 0) {
-                                         # update deg1+.net2 prior to network 1 simulation,
-                                         # as network 1's formation model depends on this
-                                         # nodal attribute
-                                         dat <- set_attr(dat, "deg1+.net2", pmin(get_degree(dat$el[[2]]), 1))
-                                       } else if (network == 1) {
-                                         # update deg1+.net1 prior to network 2 simulation,
-                                         # as network 2's formation model depends on this
-                                         # nodal attribute
-                                         dat <- set_attr(dat, "deg1+.net1", pmin(get_degree(dat$el[[1]]), 1))
-                                       } else if (network == 2) {
-                                         # update deg1+.net2 after network 2 simulation,
-                                         # for summary statistics
-                                         dat <- set_attr(dat, "deg1+.net2", pmin(get_degree(dat$el[[2]]), 1))
-                                       }
-                                       return(dat)
-                                     },
+                       dat.updates = network_layer_updates,
                        nwstats.formula = multilayer("formation",
                                                     ~edges + nodefactor("deg1+.net1") + degree(0:4)))
 
